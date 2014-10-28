23697ef6544b817b3f6b64023c22f4709404d238
Kenneth Frandsen <Frandsen86@gmail.com>
2014-10-28 15:17:14 +0100

    Opsætter værdier for felterne

--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -23,8 +23,8 @@ public void game(){
 	Die dieTwo = new Die();
 	dieTwo.rollDie();
 	
-	GUI.addPlayer("Lars", 0, 0, 0, 0);	  // De tre sidste tal angiver farve pŒ bilen   
-	GUI.addPlayer("Poul", 0, 255, 255, 255); // De tre sidste tal angiver farve pŒ bilen
+	GUI.addPlayer("Lars", 0, 0, 0, 0);	  // De tre sidste tal angiver farve pï¿½ bilen   
+	GUI.addPlayer("Poul", 0, 255, 255, 255); // De tre sidste tal angiver farve pï¿½ bilen
 
 	int felt = (dieOne.faceValue + dieTwo.faceValue) - 1;
 	
@@ -34,41 +34,66 @@ public void game(){
 	GUI.addPlayer("PlayerOne", PlayerOnePoints);
 	GUI.addPlayer("PlayerTwo", PlayerTwoPoints);
 	
-	
+	Player player1 = new Player("PlayerOne");
+	Player player2 = new Player("PlayerTwo");
+			
 int trow=0;
 	
 	if(trow==2){
 		System.out.println("Tower");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
+		
 	}
 	if(trow==3){
 		System.out.println("Crater");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==4){
 		System.out.println("Palace gates");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==5){
 		System.out.println("Cold Desert");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==6){
 		System.out.println("Walled city");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==7){
 		System.out.println("Monastrery");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==8){
 		System.out.println("Black cave");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==9){
 		System.out.println("Huts in the mountain");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==10){
 		System.out.println("The Werewall");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==11){
 		System.out.println("The pit");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 	if(trow==12){
 		System.out.println("The Goldmine");
+		player1.playerAcc.deposit(250);
+		player2.playerAcc.deposit(250);
 	}
 
 }
