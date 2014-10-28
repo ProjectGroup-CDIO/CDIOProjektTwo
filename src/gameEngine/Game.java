e61f0b543440321cffe9880546397af93dec69ea
Peter Asmussen <pasmussen1993@gmail.com>
2014-10-28 14:16:04 +0100

    Added variables and GUI playerOne and playerTwo

--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -4,7 +4,19 @@ import boundaryToMatador.GUI;
 
 public class Game {
 	
-	//test - GUI
+	int PlayerOnePoints = 1000;
+	int PlayerTwoPoints = 1000;
+	
+	int RollOne = 0;
+	int RollTwo = 0;
+	
+	boolean playerOne = true;
+	boolean playerTwo = false;
+	boolean game = true;
+	
+	static String Felt = "";
+	int feltPoint = 0;
+	
 
 public void game(){
 	GUI.create("fields2.txt");
@@ -16,6 +28,8 @@ public void game(){
 	
 	GUI.setDice(dieOne.faceValue, dieTwo.faceValue);
 	
+	GUI.addPlayer("PlayerOne", PlayerOnePoints);
+	GUI.addPlayer("PlayerTwo", PlayerTwoPoints);
 	
 	GUI.addPlayer("Lars", 0, 0, 0, 0);
 	GUI.addPlayer("Poul", 0, 255, 255, 255);
