45dc485271568f204ac7c1258ade7819f98e2f33
Anders <s140996@student.dtu.dk>
2014-10-28 13:48:21 +0100

    Oprette dieOne og dieTwo + test af GUI output

--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -3,13 +3,22 @@ package gameEngine;
 import boundaryToMatador.GUI;
 
 public class Game {
-	//test
+	
+	//test - GUI
 
 public void Game(){
 	GUI.create("fields2.txt");
-
-	GUI.addPlayer("Lars", 20);
 	
+	Die dieOne = new Die();
+	dieOne.rollDie();
+	Die dieTwo = new Die();
+	dieTwo.rollDie();
+	
+	GUI.setDice(dieOne.faceValue, dieTwo.faceValue);
+	
+	
+	GUI.addPlayer("Lars", 0, 255, 0, 0);
+	GUI.addPlayer("Poul", 0, 0, 0, 255);
 }
 	
 }
