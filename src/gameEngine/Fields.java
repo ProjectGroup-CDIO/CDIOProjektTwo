250a544f0d7850fe8cac219c2842206029c7e41c
Kenneth Frandsen <Frandsen86@gmail.com>
2014-10-29 12:54:45 +0100

    fields metode forsøgt oprettet

--- src/gameEngine/Fields.java
+++ src/gameEngine/Fields.java
@@ -1,75 +1,120 @@
 package gameEngine;
 
 public class Fields {
-	
-	int trow = 2;
-	String i ="";
-		{
 
-	if(i.equals("Roll Dice")){
-		Player player1 = new Player("PlayerOne");
-		Player player2 = new Player("PlayerTwo");
+	Player player1 = new Player("PlayerOne");
+	Player player2 = new Player("PlayerTwo");
 
-		if(trow==2){
-			System.out.println("Tower");
-			player1.playerAcc.deposit(250);
-			player2.playerAcc.deposit(250);
-			
-		}
-		if(trow==3){
-			System.out.println("Crater");
-			player1.playerAcc.withdraw(200);
-			player2.playerAcc.withdraw(200);
+
+
+
+	public String field(boolean p1, boolean p2, int trow, String i){ 
+
+		if(i.equals("Roll Dice")){
+
+			if(trow==2){
+				if(p1){
+					player1.playerAcc.deposit(250);
+				}
+				if(p2){
+					player2.playerAcc.deposit(250);
+				}
+				return "Tower";
+			}
+			if(trow==3){
+				if(p1){
+					player1.playerAcc.withdraw(200);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(200);
+				}
+				return "Crater";
+			}
+			if(trow==4){
+				if(p1){
+					player1.playerAcc.withdraw(100);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(100);
+				}
+				return "Palace gates";
+			}
+			if(trow==5){
+				if(p1){
+					player1.playerAcc.withdraw(20);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(20);
+				}
+				return "Cold Desert";
+			}
+			if(trow==6){
+				if(p1){
+					player1.playerAcc.deposit(180);
+				}
+				if(p2){
+					player2.playerAcc.deposit(180);
+				}
+				return "Walled city";
+			}
+			if(trow==7){
+				if(p1){
+					player1.playerAcc.withdraw(0);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(0);
+				}
+				return "Monastrery";
+			}
+			if(trow==8){
+				if(p1){
+					player1.playerAcc.withdraw(70);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(70);
+				}
+				return "Black cave";
+			}
+			if(trow==9){
+				if(p1){
+					player1.playerAcc.withdraw(60);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(60);
+				}
+				return "Huts in the mountain";
+			}
+			if(trow==10){
+				if(p1){
+					player1.playerAcc.withdraw(80);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(80);
+				}
+				return "The Werewall";
+			}
+			if(trow==11){
+				if(p1){
+					player1.playerAcc.withdraw(90);
+				}
+				if(p2){
+					player2.playerAcc.withdraw(90);
+				}
+				return "The pit";
+			}
+			if(trow==12){
+				if(p1){
+					player1.playerAcc.deposit(650);
+				}
+				if(p2){
+					player2.playerAcc.deposit(650);
+				}
+				return "The Goldmine";
+			}
 
 		}
-		if(trow==4){
-			System.out.println("Palace gates");
-			player1.playerAcc.withdraw(100);
-			player2.playerAcc.withdraw(100);
-		}
-		if(trow==5){
-			System.out.println("Cold Desert");
-			player1.playerAcc.withdraw(20);
-			player2.playerAcc.withdraw(20);
-		}
-		if(trow==6){
-			System.out.println("Walled city");
-			player1.playerAcc.deposit(180);
-			player2.playerAcc.deposit(180);
-		}
-		if(trow==7){
-			System.out.println("Monastrery");
-			player1.playerAcc.withdraw(0);
-			player2.playerAcc.withdraw(0);
-		}
-		if(trow==8){
-			System.out.println("Black cave");
-			player1.playerAcc.withdraw(70);
-			player2.playerAcc.withdraw(70);
-		}
-		if(trow==9){
-			System.out.println("Huts in the mountain");
-			player1.playerAcc.withdraw(60);
-			player2.playerAcc.withdraw(60);
-		}
-		if(trow==10){
-			System.out.println("The Werewall");
-			player1.playerAcc.withdraw(80);
-			player2.playerAcc.withdraw(80);
-		}
-		if(trow==11){
-			System.out.println("The pit");
-			player1.playerAcc.withdraw(90);
-			player2.playerAcc.withdraw(90);
-		}
-		if(trow==12){
-			System.out.println("The Goldmine");
-			player1.playerAcc.deposit(650);
-			player2.playerAcc.deposit(650);
-		}
+		else return "Skriv Roll Dice";
 	}
-}
-
 
 }
 
